069dcd53459ca0d7a6cc13be5c7e00db5ed32de1
PauloNobre <paulinho140@gmail.com>
2016-01-18 10:16:44 -0300

    Correções Defeito #2635

--- aplicacoes/gal_npi/src/main/webapp/resources/js/gal.js
+++ aplicacoes/gal_npi/src/main/webapp/resources/js/gal.js
@@ -260,3 +260,22 @@ function getAppName() {
 	url = url.split("/");
 	return url[1];
 }
+
+/*mostra a quantidade de exemplares que um titulo possui*/
+$(".open-AddQtdExemplares").on("click", function() {
+	var acervo = $(this).data('id');
+	var mensagem;
+	if (acervo > 0) {
+		mensagem = "Esse título possui " + acervo + " exemplares, tem certeza de que deseja exclui-lo?";
+	} else {
+		mensagem = "Tem certeza de que deseja excluir esse título?";
+	}
+	$("#mensagem").text(mensagem);
+});
+
+/*mostra o codigo do exemplar ao tentar exclui-lo*/
+$(".open-CodigoExemplar").on("click", function() {
+	var codigo = $(this).data('id');
+	var mensagem = "Tem certeza de que deseja excluir o exemplar" + codigo + " ?";
+	$("#mensagem").text(mensagem);
+});
