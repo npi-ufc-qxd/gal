9a41e935228c4a8b6dec4e8a6eb1d3ca15a0c5f8
LucasSales <lucasalesvieira2@gmail.com>
2016-04-26 11:58:49 -0300

    Estudando Envers para auditar a classe título

--- aplicacoes/gal_npi/src/main/java/br/ufc/npi/gal/auditoria/AuditoriaTitulo.java
+++ aplicacoes/gal_npi/src/main/java/br/ufc/npi/gal/auditoria/AuditoriaTitulo.java
@@ -1,24 +1,24 @@
 package br.ufc.npi.gal.auditoria;
 
-import java.util.Date;
-
 import javax.persistence.Entity;
-import javax.persistence.GeneratedValue;
 import javax.persistence.Id;
 
 import org.hibernate.envers.DefaultRevisionEntity;
 import org.hibernate.envers.RevisionEntity;
-import org.hibernate.envers.RevisionNumber;
-import org.hibernate.envers.RevisionTimestamp;
-
 
 @Entity
 @RevisionEntity(InformacoeRevisaoTitulo.class)
 public class AuditoriaTitulo extends DefaultRevisionEntity{
-	@RevisionNumber
 	@Id
-	@GeneratedValue
-	private int id;
-	@RevisionTimestamp
-	Date data;
+    private static final long serialVersionUID = 0829486158L;
+
+    private String username;
+
+    public String getUsername() {
+        return this.username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
 }
--- aplicacoes/gal_npi/src/main/java/br/ufc/npi/gal/auditoria/InformacoeRevisaoTitulo.java
+++ aplicacoes/gal_npi/src/main/java/br/ufc/npi/gal/auditoria/InformacoeRevisaoTitulo.java
@@ -5,9 +5,10 @@ import org.hibernate.envers.RevisionListener;
 public class InformacoeRevisaoTitulo implements RevisionListener{
 
 	@Override
-	public void newRevision(Object arg0) {
+	public void newRevision(Object revisionEntity) {
 		// TODO Auto-generated method stub
-		
+		AuditoriaTitulo revison = (AuditoriaTitulo) revisionEntity;
+		revison.setUsername("teste");
 	}
 
 }
